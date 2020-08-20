b7a4901dfc5299a22ba846503460ea5784eed3d1
admin <admin@TeamCity>
2020-08-20 17:09:10 +0300

    TeamCity change in 'Versioneddsl (1)' project: runners of 'Build' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
@@ -35,6 +36,13 @@ object Build : BuildType({
 
     vcs {
         root(DslContext.settingsRoot)
+    }
+
+    steps {
+        script {
+            name = "STEP NAME 111"
+            scriptContent = """echo("test")"""
+        }
     }
 
     triggers {
